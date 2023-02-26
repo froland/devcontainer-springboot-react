f71b19a695e57cf5662eb5807964aeff0b385f98
Francois Roland <froland@froland.be>
2023-02-26 00:31:08 +0000

    Remove unused imports.

--- server/src/main/java/be/eafcuccle/server/ApiController.java
+++ server/src/main/java/be/eafcuccle/server/ApiController.java
@@ -1,11 +1,9 @@
 package be.eafcuccle.server;
 
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
